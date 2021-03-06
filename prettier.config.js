e1b3ab29a40b2c31d214980918ca3214616220f0
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-03-05 23:04:29 -0600

    Svgs added

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  traillingComma: 'all',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+}
